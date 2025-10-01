df955c32894e7cd9d27b2ec677529f287ab40171
Simon Kojima <simon.kojima@inria.fr>
2025-10-01 15:55:48 +0200

    update

--- souko/proc/mod_numpy.py
+++ souko/proc/mod_numpy.py
@@ -8,7 +8,7 @@ def pick_ch(data, ch, ch_names, axis=0):
     raise ValueError()
 
 
-def crop(data, tmin, tmax, times, include_tmax=True):
+def crop(data, tmin, tmax, times, axis=0, include_tmax=True, return_times=True):
     diff = times - tmin
     Nmin = np.argmin(np.absolute(diff))
 
@@ -16,12 +16,28 @@ def crop(data, tmin, tmax, times, include_tmax=True):
     Nmax = np.argmin(np.absolute(diff))
 
     if include_tmax:
-        return data[Nmin : (Nmax + 1)], times[Nmin : (Nmax + 1)]
+        Nmax += 1
+
+    times= times[Nmin:Nmax]
+
+    if axis==0:
+        data= data[Nmin:Nmax]
+    elif axis==1:
+        data=data[:, Nmin:Nmax]
+    elif axis==2:
+        data=data[:, :, Nmin:Nmax]
     else:
-        return data[Nmin:Nmax], times[Nmin:Nmax]
+        raise NotImplementedError()
+
+    if return_times:
+        return data, times
+    else:
+        return data
 
 
-def crop_multichannels(data, tmin, tmax, times, include_tmax=True):
+
+def crop_multichannels(data, tmin, tmax, times, axis=1, include_tmax=True):
+    """
     new_data = []
     for c in range(data.shape[0]):
         d, t = crop(
@@ -29,8 +45,12 @@ def crop_multichannels(data, tmin, tmax, times, include_tmax=True):
         )
         new_data.append(d)
     new_data = np.stack(new_data, axis=0)
+    """
+    d, t = crop(
+        data, tmin=tmin, tmax=tmax, times=times, include_tmax=include_tmax, axis=axis
+    )
 
-    return new_data, t
+    return d, t
 
 
 def drop_bad(data, min=-200, max=200, verbose=False):
